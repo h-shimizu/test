7c9cc33e3ebeaa0aefdb91378bbf00b369fe183d
Homare Shimizu <homare@dova.co.jp>
2015-01-17 09:14:43 +0900

    first commit

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+SERVICE_PATH=/usr/sbin/service
+
+#----------------------------------------
+# Start Any Service
+#----------------------------------------
+# for NTP
+${SERVICE_PATH} ntp start
+
+# for MySQL
+${SERVICE_PATH} mysql start
+
+# for Zabbix Server
+${SERVICE_PATH} zabbix-server start
+
+# for Zabbix Agent
+${SERVICE_PATH} zabbix-agent start
+
+# for apache (Need FORGROUND)
+/usr/sbin/apache2 -k start -D FOREGROUND 
